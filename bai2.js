0fd4dcb7dd99e995c712a79406a4dca998566309
Quang Phu <quangphu1242000@gmail.com>
2025-07-12 11:00:26 +0700

    first commit

---
+++ bai2.js
@@ -0,0 +1,19 @@
+// khai báo hằng: const tenHang = giaTri; ( hằng không thai đổi dược giá trị)
+const MyConst = 4; // đúng
+const _MyConst = 4; // đúng
+const __MyConst = 4; // đúng
+const MyConst1 = 4; // đúng
+const _MyConst1 = 4; // đúng
+const __MyConst1 = 4; // đúng
+//const 90MyConst1 = 4; // sai
+//const -MyConst1 = 4; // sai
+
+// khai báo biến: let tenBien = giaTri; ( biến thay đổi được giá trị)
+// var tenBien = giaTri;
+var a = 4;
+let b = 5;
+
+// in ra màng hình
+console.log(a); // 4 (in ra trong bản console trong trình duyệt)
+document.write(a); // 4 in ra trực tiếp vào trang web
+document.writeln(b); // 5 in ra trực tiếp vào trang web và xuống dòng
